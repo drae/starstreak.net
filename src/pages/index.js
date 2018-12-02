ecfd647c14b89125ce4db2b0388e2d3ca029b624
Drae <paul@starstreak.net>
2018-12-02 03:01:42 +0000

    Size and position

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Star from '../assets/images/starstreak-hilight.svg'
 
 const IndexPage = () => (
   <Layout>
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
-      <Star style={{ width: '85%' }} />
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', position: 'relative', overflow: 'hidden' }}>
+      <Star style={{ width: '100%', position: 'absolute' }} />
     </div>
   </Layout>
 )
