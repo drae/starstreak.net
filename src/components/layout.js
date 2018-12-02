327343346d836abe37e45d7d4e225e8a363f0d18
Drae <paul@starstreak.net>
2018-12-02 02:45:18 +0000

    Show the basic starstreak svg

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
-//import './layout.css'
+import '../assets/scss/main.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,7 +27,6 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div>
           {children}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import Star from '../assets/images/starstreak-hilight.svg'
 
 const IndexPage = () => (
   <Layout>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+      <Star style={{ width: '85%' }} />
+    </div>
   </Layout>
 )
 
