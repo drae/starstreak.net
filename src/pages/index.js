b0697b977326276eeccde4a919ccbaefd9026f6f
Drae <paul@starstreak.net>
2018-12-02 15:04:13 +0000

    Update to SVG star

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Star from '../assets/images/starstreak-hilight.svg'
 const IndexPage = () => (
   <Layout>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh', position: 'relative', overflow: 'hidden' }}>
-      <Star style={{ width: '100%', position: 'absolute' }} />
+      <Star style={{ maxHeight: '85%',maxWidth: '85%', position: 'absolute' }} />
     </div>
   </Layout>
 )
